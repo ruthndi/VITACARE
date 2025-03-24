fac836d169a607f574edb5aa27b5fbaeabb3a011
ruthndi <ruthndi08@gmail.com>
2025-03-24 09:42:08 +0000

    ajout du projet

---
+++ JS/menu.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menu = document.getElementById('menu');
+    const closeButton = document.getElementById('close');
+    const menuIcon = document.getElementById('menu-icon');
+
+    menuIcon.addEventListener('click', function() {
+        menu.classList.remove('hidden'); // Affiche le menu
+    });
+
+    closeButton.addEventListener('click', function() {
+        menu.classList.add('hidden'); // Cache le menu
+        menu.classList.remove('fixed', 'top-0', 'right-0', 'w-auto'); // retire les classes de fixation
+        menu.classList.add('absolute', 'w-auto', 'top-0', 'right-0'); // remet les classes de positionnement initial
+        closeButton.style.display = 'block'; // affiche le bouton close
+    });
+});
+
+        
